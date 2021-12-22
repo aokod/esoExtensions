c32dde4975d4d684f712a352eb1dc120cadb9f1c
grntbg <13023526+grntbg@users.noreply.github.com>
2021-12-22 16:36:47 -0600

    Run the upgrade() function, and remove borders in widescreen mode

--- Plugins/WidescreenMode/plugin.php
+++ Plugins/WidescreenMode/plugin.php
@@ -6,7 +6,7 @@ class WidescreenMode extends Plugin {
 
 var $id = "WidescreenMode";
 var $name = "Widescreen Mode";
-var $version = "1.0";
+var $version = "1.1";
 var $description = "Allows users to set widescreen mode";
 var $author = "GigaHacer";
 
@@ -14,30 +14,22 @@ function init()
 {
 	parent::init();
 
-    global $config;
-
-    // Check if the database has the column the plugin requires.
-	if (!$this->eso->db->numRows("SHOW COLUMNS FROM {$config["tablePrefix"]}members LIKE 'widescreen'")) {
-        // If it doesn't, create it.
-		$this->eso->db->query("ALTER TABLE {$config["tablePrefix"]}members ADD COLUMN widescreen tinyint(1) NOT NULL default '0'");
-	}
-
 	// Language definitions.
-	$this->eso->addLanguage("widescreenMode", "Widescreen Mode");
+	$this->eso->addLanguage("widescreenMode", "Use the full width of your browser window");
 
 	// If we're on the settings view, add the skin settings!
 	if ($this->eso->action == "settings") {
 		$this->eso->controller->addHook("init", array($this, "addWidescreenSettings"));
 	}
 
-    $this->eso->addHook("init", array($this, "setWidescreen"));
+	$this->eso->addHook("init", array($this, "setWidescreen"));
 
 }
 
 // This is the part where we actually give the user the CSS if they've checked the setting.
 function setWidescreen()
 {
-    if (!empty($this->eso->user["widescreen"])) $this->eso->addCSS("plugins/WidescreenMode/widescreen.css");
+	if (!empty($this->eso->user["widescreen"])) $this->eso->addCSS("plugins/WidescreenMode/widescreen.css");
 }
 
 // This is the part where we add the setting to the settings screen. Fun!
@@ -47,10 +39,20 @@ function addWidescreenSettings(&$settings)
 
 	$settings->addToForm("settingsOther", array(
 		"id" => "widescreen",
-        "html" => "<label for='widescreenMode' class='checkbox'>{$language["widescreenMode"]}</label> <input id='widescreen' type='checkbox' class='checkbox' name='widescreen' value='1' " .  (!empty($this->eso->user["widescreen"]) ? "checked='checked' " : "") . "/>",
+		"html" => "<label for='widescreenMode' class='checkbox'>{$language["widescreenMode"]}</label> <input id='widescreen' type='checkbox' class='checkbox' name='widescreen' value='1' " .  (!empty($this->eso->user["widescreen"]) ? "checked='checked' " : "") . "/>",
 		"databaseField" => "widescreen",
 		"checkbox" => true
 	), 250);
+}
+
+function upgrade($oldVersion)
+{
+	global $config;
+
+	if (!$this->eso->db->numRows("SHOW COLUMNS FROM {$config["tablePrefix"]}members LIKE 'widescreen'")) {
+		$this->eso->db->query("ALTER TABLE {$config["tablePrefix"]}members ADD COLUMN widescreen tinyint(1) NOT NULL default '0'");
+	}
+
 }
 
 }
